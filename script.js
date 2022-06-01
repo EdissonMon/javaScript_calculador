2f1a54af28223910a3d1f36207ec9232427e5320
Edisson Montealegre <106637949+EdissonMon@users.noreply.github.com>
2022-05-31 20:37:46 -0500

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 //Botones
 
 function valor(x) {
-    document.getElementById('display').value += x ;
+  document.getElementById("display").value += x;
 }
 
 //Borrar
 function borrarDisplay(y) {
-    document.getElementById('display').value = y;
+  document.getElementById("display").value = y;
 }
 
 //Resultado
 function calResultado() {
-    var result = eval(document.getElementById('display').value);
-    document.getElementById('display').value = result;
+  var result = eval(document.getElementById("display").value);
+  document.getElementById("display").value = result;
 }
+
+//Other
